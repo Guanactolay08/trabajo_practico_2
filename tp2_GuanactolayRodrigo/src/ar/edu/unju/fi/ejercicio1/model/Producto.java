00755db162e104397371e23f26a88a388335d3bc
Rodrigo <Rodrigo@DESKTOP-2SFTDIT>
2024-04-22 21:06:14 -0300

    Se completa la clase Producto.

--- tp2_GuanactolayRodrigo/src/ar/edu/unju/fi/ejercicio1/model/Producto.java
+++ tp2_GuanactolayRodrigo/src/ar/edu/unju/fi/ejercicio1/model/Producto.java
@@ -1,5 +1,78 @@
 package ar.edu.unju.fi.ejercicio1.model;
 
 public class Producto {
+	private int codigo;
+	private String descripcion;
+	private float precioUnitario;
+	private OrigenFabricacion origen;
+	private Categoria categoria;
+	
+	public enum OrigenFabricacion{
+		ARGENTINA, CHINA, BRASIL, URUGUAY
+	}
+	public enum Categoria{
+		TELEFONIA,INFORMATICA, ELECTROHOGAR, HERRAMIENTAS
+	}
+	
+	public Producto() {
+		// TODO Auto-generated constructor stub
+	}
 
+	public Producto(int codigo, String descripcion, float precioUnitario, OrigenFabricacion origen,
+			Categoria categoria) {
+		this.codigo = codigo;
+		this.descripcion = descripcion;
+		this.precioUnitario = precioUnitario;
+		this.origen = origen;
+		this.categoria = categoria;
+	}
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+	public float getPrecioUnitario() {
+		return precioUnitario;
+	}
+
+	public void setPrecioUnitario(float precioUnitario) {
+		this.precioUnitario = precioUnitario;
+	}
+
+	public OrigenFabricacion getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(OrigenFabricacion origen) {
+		this.origen = origen;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	@Override
+	public String toString() {
+		return "Producto [Codigo=" + codigo + ", Descripcion=" + descripcion + ", PrecioUnitario=" + precioUnitario
+				+ ", Origen=" + origen + ", Categoria=" + categoria + "]";
+	}
+	
+	
+	
 }
