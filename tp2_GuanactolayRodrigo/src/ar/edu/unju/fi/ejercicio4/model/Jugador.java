b7c088ee33cec639b5ff68ca1eb3e58a37dabbab
Rodrigo <Rodrigo@DESKTOP-2SFTDIT>
2024-04-27 20:33:34 -0300

    Se completa la clase Jugador y el enumerado Posicion.

--- tp2_GuanactolayRodrigo/src/ar/edu/unju/fi/ejercicio4/model/Jugador.java
+++ tp2_GuanactolayRodrigo/src/ar/edu/unju/fi/ejercicio4/model/Jugador.java
@@ -1,5 +1,111 @@
 package ar.edu.unju.fi.ejercicio4.model;
 
-public class Jugador {
+import java.time.LocalDate;
 
+import ar.edu.unju.fi.ejercicio4.constantes.Posicion;
+
+public class Jugador {
+	private String nombre;
+	private String apellido;
+	private LocalDate fechaNacimiento;
+	private String nacionalidad;
+	private float estatura;
+	private float peso;
+	private Posicion posicion;
+	
+	public Jugador() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Jugador(String nombre, String apellido, LocalDate fechaNacimiento, String nacionalidad, float estatura,
+			float peso, Posicion posicion) {
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.fechaNacimiento = fechaNacimiento;
+		this.nacionalidad = nacionalidad;
+		this.estatura = estatura;
+		this.peso = peso;
+		this.posicion = posicion;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+
+	public float getEstatura() {
+		return estatura;
+	}
+
+	public void setEstatura(float estatura) {
+		this.estatura = estatura;
+	}
+
+	public float getPeso() {
+		return peso;
+	}
+
+	public void setPeso(float peso) {
+		this.peso = peso;
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	public void setPosicion(Posicion posicion) {
+		this.posicion = posicion;
+	}
+
+	@Override
+	public String toString() {
+		return "Jugador [Nombre=" + nombre + ", Apellido=" + apellido + ", Fecha de Nacimiento=" + fechaNacimiento
+				+ ", Nacionalidad=" + nacionalidad + ", Estatura=" + estatura + ", Peso=" + peso + ", Posicion="
+				+ posicion + "]";
+	}
+	/*
+	 * METODO PARA CALCULAR LA EDAD, RETORNA UN VALOR ENTERO.
+	 */
+	
+	public int calcularEdad() {
+		LocalDate fechaActual = LocalDate.now();
+		int añoActual = fechaActual.getYear();
+		int edad = añoActual - this.fechaNacimiento.getYear();
+		if (this.fechaNacimiento.getMonthValue() > fechaActual.getMonthValue()) {
+				edad=edad-1;
+		}else {
+			if(this.fechaNacimiento.getMonthValue() == fechaActual.getMonthValue() && (this.fechaNacimiento.getDayOfMonth()>fechaActual.getDayOfMonth()) ) {	
+					edad=edad-1;
+			}
+		}	
+		return edad;
+	}
+	
 }
