d11d436ee7f959aa406ee6c48291367521732bf8
Rodrigo <Rodrigo@DESKTOP-2SFTDIT>
2024-04-29 20:30:00 -0300

    Se crean paquetes, clases e interfaz para el ejercicio 5.

---
+++ tp2_GuanactolayRodrigo/src/ar/edu/unju/fi/ejercicio5/model/PagoTarjeta.java
@@ -0,0 +1,58 @@
+package ar.edu.unju.fi.ejercicio5.model;
+
+import java.time.LocalDate;
+
+import ar.edu.unju.fi.ejercicio5.interfaces.Pago;
+
+public class PagoTarjeta implements Pago {
+	private String numeroTarjeta;
+	private LocalDate fechaPago;
+	private double montoPagado;
+	
+	@Override
+	public void realizarPago(double monto) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void imprimirRecibo() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public PagoTarjeta() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PagoTarjeta(String numeroTarjeta, LocalDate fechaPago, double montoPagado) {
+		this.numeroTarjeta = numeroTarjeta;
+		this.fechaPago = fechaPago;
+		this.montoPagado = montoPagado;
+	}
+
+	public String getNumeroTarjeta() {
+		return numeroTarjeta;
+	}
+
+	public void setNumeroTarjeta(String numeroTarjeta) {
+		this.numeroTarjeta = numeroTarjeta;
+	}
+
+	public LocalDate getFechaPago() {
+		return fechaPago;
+	}
+
+	public void setFechaPago(LocalDate fechaPago) {
+		this.fechaPago = fechaPago;
+	}
+
+	public double getMontoPagado() {
+		return montoPagado;
+	}
+
+	public void setMontoPagado(double montoPagado) {
+		this.montoPagado = montoPagado;
+	}
+
+}
